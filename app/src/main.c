9b10b52f2020137c839f02287dc6907c90ce7688
Ara Gevorgyan <aragev@gmail.com>
2026-08-22 23:45:48 +0400

    Lesson 2 task 1

--- app/src/main.c
+++ app/src/main.c
@@ -5,53 +5,30 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
 #define STACK_SIZE 1024
 
-#define PRIO_L 7
-#define PRIO_M 5
-#define PRIO_H 3
-#define PRIO_COOP -1
+#define PRIO 5
+#define ITERATIONS 1000000
 
-void t_low_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        LOG_INF("T_LOW running");
-        k_msleep(300);
-    }
-}
+int num = 0;
 
-void t_med_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        LOG_INF("T_MED running");
-        k_msleep(200);
-    }
-}
+K_MUTEX_DEFINE(my_mutex);
 
-void t_high_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        LOG_INF("T_HIGH running");
-        k_msleep(100);
-    }
-}
 
-void t_coop_fn(void *p1, void *p2, void *p3)
+void inc_thread_fn(void *p1, void *p2, void *p3)
 {
-    for (size_t i = 0; i < 5; i++)
+    for (size_t i = 0; i < ITERATIONS; i++)
     {
-        LOG_INF("T_COOP running");
+        k_mutex_lock(&my_mutex, K_FOREVER);
+        num++;
+        k_mutex_unlock(&my_mutex);
     }
-    k_yield();
+    LOG_INF("num: %d\n", num);
 }
 
 
-K_THREAD_DEFINE(t_high, STACK_SIZE, t_high_fn,
-                NULL, NULL, NULL, PRIO_H, 0, 0);
-K_THREAD_DEFINE(t_med, STACK_SIZE, t_med_fn,
-                NULL, NULL, NULL, PRIO_M, 0, 0);
-K_THREAD_DEFINE(t_low, STACK_SIZE, t_low_fn,
-                NULL, NULL, NULL, PRIO_L, 0, 0);
-K_THREAD_DEFINE(t_coop, STACK_SIZE, t_coop_fn,
-                NULL, NULL, NULL, PRIO_COOP, 0, 0);
+K_THREAD_DEFINE(inc_thread_a, STACK_SIZE, inc_thread_fn,
+                NULL, NULL, NULL, PRIO, 0, 0);
+K_THREAD_DEFINE(inc_thread_b, STACK_SIZE, inc_thread_fn,
+                NULL, NULL, NULL, PRIO, 0, 0);
 
 
 int main(void)
